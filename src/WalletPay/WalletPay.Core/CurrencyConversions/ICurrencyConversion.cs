083eb4cdd6693d6b17e08618d342d90e82d0de3d
wargerun <wargerun@gmail.com>
2020-11-01 23:36:48 +0300

    xml-doc on ICurrencyConversion interface

--- src/WalletPay/WalletPay.Core/CurrencyConversions/ICurrencyConversion.cs
+++ src/WalletPay/WalletPay.Core/CurrencyConversions/ICurrencyConversion.cs
@@ -2,6 +2,13 @@
 {
     public interface ICurrencyConversion
     {
+        /// <summary>
+        /// Метод, получения конвертированной суммы из одной валюты в другую
+        /// </summary>
+        /// <param name="fromCurrency"></param>
+        /// <param name="toCurrency"></param>
+        /// <param name="amount"></param>
+        /// <returns></returns>
         decimal CurrencyConvert(string fromCurrency, string toCurrency, decimal amount);
     }
 }
